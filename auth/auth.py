93269da632ef94008b8dee1e678506b4a2d5d7d3
yuri yuri <sharikov@gmail.com>
2024-01-23 18:33:56 +0300

    Authenticate base"

--- auth/auth.py
+++ auth/auth.py
@@ -1,9 +1,11 @@
 from fastapi_users.authentication import CookieTransport, AuthenticationBackend
 from fastapi_users.authentication import JWTStrategy
 
+from config import SECRET
+
+SECRET = SECRET
 cookie_transport = CookieTransport(cookie_name="ruchamp", cookie_max_age=3600)
 
-SECRET = "58b3a17a-d148-4d52-8926-c626b79902ad"
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
