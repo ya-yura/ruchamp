268fa24511abe0388744c3677c09dfc71b47dc7f
Сергей Емельянов <3201888@mail.ru>
2024-01-24 06:16:04 +0300

    create user work

---
+++ auth/auth.py
@@ -0,0 +1,16 @@
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend
+from fastapi_users.authentication import JWTStrategy
+
+from config import SECRET
+
+cookie_transport = CookieTransport(cookie_name="ruchamp", cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
