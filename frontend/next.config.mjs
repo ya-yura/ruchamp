37cfb93edf5fac9bfeb862d16f161e552a4823ab
Aleksei Smirnov <sibusky@yandex.ru>
2024-03-06 14:51:36 +0400

    feat: install and configure main dependencies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
+import million from "million/compiler";
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+};
+ 
+const millionConfig = {
+  auto: true,// if you're using RSC: auto: { rsc: true },
+};
+ 
+export default million.next(nextConfig, millionConfig);
