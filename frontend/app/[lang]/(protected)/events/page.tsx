8c7c3b7bca1629ea6c84c3074fc963479f0538c0
Aleksei Smirnov <sibusky@yandex.ru>
2024-04-15 13:18:08 +0400

    feat: add random images on event cards and change date appearance

--- frontend/app/[lang]/(protected)/events/page.tsx
+++ frontend/app/[lang]/(protected)/events/page.tsx
@@ -15,25 +15,9 @@ export default async function Events() {
   token = session.token;
   events = await eventsApi.getEvents(token);
 
-  const mockEvent = {
-    name: 'Art Exhibition: Reflections',
-    start_datetime: '2024-05-10T10:00:00',
-    location: 'Art Gallery, 123 Main St, Cityville',
-    event_order:
-      'A showcase of contemporary art exploring themes of introspection and self-discovery.',
-    geo: '40.712776, -74.005974',
-    end_datetime: '2024-05-30T18:00:00',
-    id: 5,
-    organizer_id: 2103,
-    event_system: 'exhibition',
-    image_field: 'https://placeimg.com/640/480/art',
-  };
-
-  const mockEvents = [...events, mockEvent];
-
   return (
     <Container>
-      <EventsTabs events={mockEvents} />
+      <EventsTabs events={events} />
     </Container>
   );
 }
