77707b2c7bf3dc78fe2a08061193102528be4cb1
Aleksei Smirnov <sibusky@yandex.ru>
2024-04-20 11:37:10 +0400

    feat: add adaptive for FeaturesAdditional Section on Main Page

--- frontend/components/home-page/features-additional.tsx
+++ frontend/components/home-page/features-additional.tsx
@@ -1,5 +1,6 @@
 import { ContentWraper } from '../content-wraper';
 import { OrganizationsIcon } from '../icons';
+import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 
 type TypeFeaturesAdditional = {
   icon: React.ReactNode;
@@ -17,7 +18,7 @@ const features: TypeFeaturesAdditional[] = [
     icon: (
       <OrganizationsIcon className="transition-colors group-hover:fill-white" />
     ),
-    title: 'Система с двойным выбыванием',
+    title: 'Система с двойным выбыванием',
   },
   {
     icon: (
@@ -29,13 +30,13 @@ const features: TypeFeaturesAdditional[] = [
     icon: (
       <OrganizationsIcon className="transition-colors group-hover:fill-white" />
     ),
-    title: 'Система с голосованием судей',
+    title: 'Система с голосованием судей',
   },
   {
     icon: (
       <OrganizationsIcon className="transition-colors group-hover:fill-white" />
     ),
-    title: 'Система с возвращением для 3 игроков',
+    title: 'Система с возвращением для 3 игроков',
   },
   {
     icon: (
@@ -47,16 +48,32 @@ const features: TypeFeaturesAdditional[] = [
 
 export function FeaturesAdditional() {
   return (
-    <section className="w-full bg-[#0A0A0A] px-[72px] py-14">
+    <section className="w-full bg-[#0A0A0A] px-0 py-11 sm:py-14 sm:px-7 md:px-10 lg:px-[72px]">
       <ContentWraper>
-        <ul className="grid w-full grid-flow-dense grid-cols-4 gap-6">
-          <div className="col-start-3 col-end-5 flex flex-col justify-center gap-2">
-            <h3 className="to-53% bg-gradient-to-r from-[#95CEFF] from-25%  to-[#0F6CBD] bg-clip-text text-[40px] font-bold leading-[47px] tracking-wide text-transparent">
+        <ScrollArea className="block w-full whitespace-nowrap rounded-md sm:hidden">
+          <ul className="flex w-max gap-4 pb-7 pt-4">
+            {features.map((feature) => (
+              <li
+                key={feature.title}
+                className="group flex h-[212px] w-[255px] cursor-default flex-col rounded-lg bg-[#0D0D0D] p-8 transition-colors hover:bg-[#0F6CBD] first-of-type:ml-4 last-of-type:mr-4"
+              >
+                <div className="mb-4 w-fit">{feature.icon}</div>
+                <h4 className="mb-2 w-full text-wrap text-lg font-semibold text-[#C9C9C9] group-hover:text-white">
+                  {feature.title}
+                </h4>
+              </li>
+            ))}
+          </ul>
+          <ScrollBar className="hidden" orientation="horizontal" />
+        </ScrollArea>
+        <ul className="flex w-full flex-col sm:grid sm:grid-flow-dense sm:grid-cols-4 sm:gap-4 lg:gap-6">
+          <div className="flex flex-col justify-center gap-4 sm:gap-2 mx-10 sm:mx-0 sm:col-start-3 sm:col-end-5">
+            <h3 className="to-53% bg-gradient-to-r from-[#95CEFF] from-25%  to-[#0F6CBD] bg-clip-text text-2xl font-bold leading-7 tracking-wide text-transparent sm:text-3xl md:text-4xl md:leading-[47px] lg:text-[40px]">
               Считаете,
               <br />
               что этого мало?
             </h3>
-            <p className="max-w-[440px] text-lg text-[#C9C9C9]">
+            <p className="max-w-[440px] text-[#C9C9C9] md:text-base lg:text-lg">
               Напишите нам и расскажите, чего вам не хватает? Мы стремимся
               улучшить спорт!
             </p>
@@ -64,10 +81,10 @@ export function FeaturesAdditional() {
           {features.map((feature) => (
             <li
               key={feature.title}
-              className="group flex h-[240px] w-full cursor-default flex-col rounded-lg bg-[#0D0D0D] px-8 py-10 transition-colors hover:bg-[#0F6CBD]"
+              className="group hidden w-full cursor-default flex-col rounded-lg bg-[#0D0D0D] px-2 py-3 transition-colors hover:bg-[#0F6CBD] sm:flex md:px-3 md:py-5 lg:h-[200px] lg:py-6 xl:h-[240px] xl:px-8 xl:py-10"
             >
               <div className="mb-4 w-fit">{feature.icon}</div>
-              <h4 className="mb-2 w-full text-wrap text-[23px] font-semibold text-[#C9C9C9] group-hover:text-white">
+              <h4 className="mb-2 w-full text-wrap    font-semibold text-[#C9C9C9] group-hover:text-white md:text-base lg:text-xl xl:text-[23px]">
                 {feature.title}
               </h4>
             </li>
