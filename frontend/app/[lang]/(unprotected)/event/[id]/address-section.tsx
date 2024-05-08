47269ab81b1271b45a9dc9f87c514cc2056eda33
Aleksei Smirnov <sibusky@yandex.ru>
2024-05-08 14:51:29 +0400

    feat: add map on Events Page

--- frontend/app/[lang]/(unprotected)/event/[id]/address-section.tsx
+++ frontend/app/[lang]/(unprotected)/event/[id]/address-section.tsx
@@ -6,13 +6,11 @@ import { YandexMap } from '@/components/yandex-map';
 import { TypeEvent } from '@/lib/definitions';
 
 export function AddressSection({ event }: { event: TypeEvent }) {
-  const x = +event.geo.split(',')[0];
-  const y = +event.geo.split(',')[1];
 
   return (
     <CustomSection className="flex-col items-start gap-6 pb-10">
       <H3 className="text-text-muted">Адрес</H3>
-      <YandexMap x={x} y={y} title={event.name} />
+      <YandexMap places={[event]} />
       <div className="flex flex-col">
         <p className="text-text-muted text-base font-normal">
           {event.location}
