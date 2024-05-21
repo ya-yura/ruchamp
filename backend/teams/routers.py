a7d954f26984e0b7d0a1b49a5ac02d5c9da01790
emelyanov-sergey <3201888@mail.ru>
2024-05-21 14:46:58 +0300

    .,.

--- backend/teams/routers.py
+++ backend/teams/routers.py
@@ -325,9 +325,10 @@ async def get_all_teams(
 
             members_info.append(user_info)
             i += 1
-        if captain_info is None:
-            raise HTTPException(status_code=404, detail="Captain not found")
-        team.append(captain_info[0])
+        if captain_info:
+            team.append(captain_info[0])
+        else:
+            continue
         team.append(members_info)
         result.append(team)
 
