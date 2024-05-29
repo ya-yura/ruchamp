8a2ff5bb0ad3156cfc87f443418efbb887af424e
Aleksei Smirnov <sibusky@yandex.ru>
2024-05-29 10:45:09 +0400

    fix: remove old data from Profile Page and paste temporary placeholder

--- frontend/app/[lang]/(protected)/profile/page.tsx
+++ frontend/app/[lang]/(protected)/profile/page.tsx
@@ -8,57 +8,59 @@ import { UserFormSpectator } from './user-form-spectator';
 import { UserFormSysadmin } from './user-form-sysadmin';
 import { UserFormReferee } from './user-form-referee';
 import { getSession } from '@/lib/actions';
+import { H4 } from '@/components/text';
 
 export default async function Profile() {
-  const session = await getSession();
-  const userCommonData =
-    session &&
-    session.user.find((item: object) => Object.keys(item).includes('User'))[
-      'User'
-    ];
-  const userSpecialData = () => {
-    switch (userCommonData.role_id) {
-      case 1:
-        return (
-          session &&
-          session.user.find((item: object) =>
-            Object.keys(item).includes('Athlete'),
-          )?.['Athlete']
-        );
-      case 2:
-        return (
-          session &&
-          session.user.find((item: object) =>
-            Object.keys(item).includes('EventOrganizer'),
-          )?.['EventOrganizer']
-        );
-      case 3:
-        return (
-          session &&
-          session.user.find((item: object) =>
-            Object.keys(item).includes('Spectator'),
-          )?.['Spectator']
-        );
-      case 4:
-        return (
-          session &&
-          session.user.find((item: object) =>
-            Object.keys(item).includes('Sysadmin'),
-          )?.['Sysadmin']
-        );
-      case 5:
-        return (
-          session &&
-          session.user.find((item: object) =>
-            Object.keys(item).includes('Referee'),
-          )?.['Referee']
-        );
-    }
-  };
+  // const session = await getSession();
+  // const userCommonData =
+  //   session &&
+  //   session.user.find((item: object) => Object.keys(item).includes('User'))[
+  //     'User'
+  //   ];
+  // const userSpecialData = () => {
+  //   switch (userCommonData.role_id) {
+  //     case 1:
+  //       return (
+  //         session &&
+  //         session.user.find((item: object) =>
+  //           Object.keys(item).includes('Athlete'),
+  //         )?.['Athlete']
+  //       );
+  //     case 2:
+  //       return (
+  //         session &&
+  //         session.user.find((item: object) =>
+  //           Object.keys(item).includes('EventOrganizer'),
+  //         )?.['EventOrganizer']
+  //       );
+  //     case 3:
+  //       return (
+  //         session &&
+  //         session.user.find((item: object) =>
+  //           Object.keys(item).includes('Spectator'),
+  //         )?.['Spectator']
+  //       );
+  //     case 4:
+  //       return (
+  //         session &&
+  //         session.user.find((item: object) =>
+  //           Object.keys(item).includes('Sysadmin'),
+  //         )?.['Sysadmin']
+  //       );
+  //     case 5:
+  //       return (
+  //         session &&
+  //         session.user.find((item: object) =>
+  //           Object.keys(item).includes('Referee'),
+  //         )?.['Referee']
+  //       );
+  //   }
+  // };
 
   return (
-    <Container className="min-h-fit">
-      <section className="relative mt-[-92px] flex h-[800px] w-full items-start justify-center gap-10 bg-[#0A0A0A] px-[72px] pt-[92px]">
+    <Container className="min-h-screen">
+      <H4>Страница в разработке</H4>
+      {/* <section className="relative mt-[-92px] flex h-[800px] w-full items-start justify-center gap-10 bg-[#0A0A0A] px-[72px] pt-[92px]">
         <Image
           className="opacity-50"
           src="/ru/images/event-hero-bg.jpeg"
@@ -76,13 +78,10 @@ export default async function Profile() {
         {userCommonData?.role_id === EnumUserRole['spectator'] && (
           <UserFormSpectator spectator={userSpecialData()} />
         )}
-        {/* {userCommonData?.role_id === EnumUserRole['admin'] && (
-          <UserFormSysadmin sysadmin={userSpecialData()} />
-        )} */}
         {userCommonData?.role_id === EnumUserRole['referee'] && (
           <UserFormReferee referee={userSpecialData()} />
         )}
-      </section>
+      </section> */}
     </Container>
   );
 }
