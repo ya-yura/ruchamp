013e04f71a4a1119fb194bcaebff62aeb84c4c26
Julia Zhidkova <zhidkova.julia@gmail.com>
2024-06-14 13:42:33 +0200

    fix:change layout for tournament grid

--- frontend/app/[lang]/(unprotected)/event/[id]/matches/[matchId]/grid.tsx
+++ frontend/app/[lang]/(unprotected)/event/[id]/matches/[matchId]/grid.tsx
@@ -2,7 +2,7 @@ import { ContentWraper } from '@/components/content-wraper';
 import { Tag } from '@/components/tag';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
-import { H4, PersonDescriptionOnCard } from '@/components/text';
+import { H4, H4low, PersonDescriptionOnCard } from '@/components/text';
 import { format } from 'date-fns';
 import {
   HoverCard,
@@ -65,34 +65,34 @@ export function Grid({ info, rounds }: GridProps) {
 function GridField({ rounds }: { rounds: GridRound[] }) {
   const colVariants: Record<number, string> = {
     1: 'grid-cols-1',
-    2: 'grid-cols-[repeat(2,_2fr)]',
-    3: 'grid-cols-[repeat(1,_2fr)_1fr_1fr]',
-    4: 'grid-cols-[repeat(2,_2fr)_1fr_1fr]',
-    5: 'grid-cols-[repeat(3,_2fr)_1fr_1fr]',
-    6: 'grid-cols-[repeat(4,_2fr)_1fr_1fr]',
-    7: 'grid-cols-[repeat(5,_2fr)_1fr_1fr]',
+    2: 'grid-cols-[repeat(2,_175px)]',
+    3: 'grid-cols-[repeat(1,_175px)_130px_122px]',
+    4: 'grid-cols-[repeat(2,_175px)_130px_122px]',
+    5: 'grid-cols-[repeat(3,_175px)_130px_122px]',
+    6: 'grid-cols-[repeat(4,_175px)_130px_122px]',
+    7: 'grid-cols-[repeat(5,_175px)_130px_122px]',
   };
   const mtVariants: Record<string, string> = {
     0: 'mt-[0]',
-    1: 'mt-[36px]',
-    2: 'mt-[90px]',
-    3: 'mt-[198px]',
-    4: 'mt-[414px]',
-    5: 'mt-[846px]',
-    6: 'mt-[846px]',
+    1: 'mt-[30px]',
+    2: 'mt-[89px]',
+    3: 'mt-[208px]',
+    4: 'mt-[448px]',
+    5: 'mt-[928px]',
+    6: 'mt-[928px]',
   };
 
   return (
     <ScrollArea className="-mx-4 w-screen sm:mx-0 sm:w-full">
       <div className="flex w-[1300px] flex-col gap-5 xl:w-full">
-        <ul className={cn('grid', colVariants[rounds.length])}>
+        <ul className={cn('grid px-4', colVariants[rounds.length])}>
           {rounds.map((round, index) => (
             <li key={round.name}>
               {index === rounds.length - 1 || index === rounds.length - 2 ? (
                 index === rounds.length - 1 ? (
                   <Image
                     className="mx-auto h-10 w-10"
-                    src={`/images/medals/gold.svg`}
+                    src={'/ru/images/medals/gold.svg'}
                     alt=""
                     width={213}
                     height={241}
@@ -100,14 +100,14 @@ function GridField({ rounds }: { rounds: GridRound[] }) {
                 ) : (
                   <Image
                     className="mx-auto h-10 w-10"
-                    src={`/images/medals/bronze.svg`}
+                    src={'/ru/images/medals/bronze.svg'}
                     alt=""
                     width={213}
                     height={241}
                   />
                 )
               ) : (
-                <p className="text-center text-4xl font-bold text-card-background">
+                <p className="text-center text-4xl font-bold text-card-background pr-[53px]">
                   {round.name}
                 </p>
               )}
@@ -132,6 +132,7 @@ function GridField({ rounds }: { rounds: GridRound[] }) {
               {round.fights.map((fight) => (
                 <GridCard
                   key={fight.fight_info.fight_id}
+                  fight_id={fight.fight_info.fight_id}
                   time={fight.fight_info.start_time}
                   mat_number={fight.fight_info.mat_number}
                   player_1={fight.player_1}
@@ -139,6 +140,8 @@ function GridField({ rounds }: { rounds: GridRound[] }) {
                   isFirstCol={index === 0}
                   isLastCol={index === rounds.length - 1}
                   isPreLastCol={index === rounds.length - 2}
+                  isPreSemiFinalCol={index === rounds.length - 3}
+                  roundsNumber = {rounds.length}
                   roundIndex={index}
                 />
               ))}
@@ -152,6 +155,7 @@ function GridField({ rounds }: { rounds: GridRound[] }) {
 }
 
 interface GridCardProps {
+  fight_id: number;
   time: string;
   mat_number: number;
   player_1: GridPlayer;
@@ -159,11 +163,14 @@ interface GridCardProps {
   isFirstCol?: boolean;
   isLastCol?: boolean;
   isPreLastCol?: boolean;
+  isPreSemiFinalCol?: boolean;
   roundIndex: number;
+  roundsNumber: number;
   className?: string;
 }
 
 export function GridCard({
+  fight_id,
   time,
   mat_number,
   player_1,
@@ -171,86 +178,93 @@ export function GridCard({
   isFirstCol,
   isLastCol,
   isPreLastCol,
+  isPreSemiFinalCol,
   roundIndex,
+  roundsNumber,
   className,
 }: GridCardProps) {
   const isPlayerFirstWinner = player_1.points > player_2.points;
   const isDraw = player_1.points === player_2.points;
   const arrowHeight: Record<string, string> = {
-    0: 'h-[0]',
-    1: 'h-[18px]',
-    2: 'h-[72px]',
-    3: 'h-[180px]',
-    4: 'h-[396px]',
+    0: 'h-[13px]',
+    1: 'h-[44px]',
+    2: 'h-[104px]',
+    3: 'h-[224px]',
+    4: 'h-[464px]',
     5: 'h-[828px]',
-    6: 'h-[828px]',
   };
+  const arrowTop: Record<string, string> = {
+    0: 'top-[12.5px]',
+    1: 'top-[43px]',
+    2: 'top-[103px]',
+    3: 'top-[223px]',
+    4: 'top-[463px]',
+    5: 'top-[828px]',
+  };
+  const arrowTopShift: Record<string, string> = {
+    0: 'h-[29%]',
+    1: 'h-[29%]',
+    2: 'h-[15%]',
+    3: 'h-[7%]',
+    4: 'h-[3.5%]',
+  };
+  const arrowBottomShift: Record<string, string> = {
+    0: 'h-[71%]',
+    1: 'h-[71%]',
+    2: 'h-[39%]',
+    3: 'h-[18%]',
+    4: 'h-[9%]',
+  };
+  // Вывод стрелок на бронзовый матч для соревнований,начинающихся с 1/4-1/32
+  const bronzeArrowHeight: Record<string, string> = {
+    4: 'h-[19px]',
+    5: 'h-[79px]',
+    6: 'h-[199px]',
+    7: 'h-[439px]',
+  };
+  const bronzeArrowTop: Record<string, string> = {
+    4: 'top-[18px]',
+    5: 'top-[78px]',
+    6: 'top-[198px]',
+    7: 'top-[438px]',
+  };
+  const bronzeArrowTopShift: Record<string, string> = {
+    4: 'top-[-34px]',
+    5: 'top-[-84px]',
+    6: 'top-[-201px]',
+    7: 'top-[-441px]',
+  };
+  const bronzeArrowBottomShift: Record<string, string> = {
+    4: 'top-[-53px]',
+    5: 'top-[-119px]',
+    6: 'top-[-243px]',
+    7: 'top-[-487px]',
+  };
+  //
   const spaceHeight: Record<string, string> = {
-    0: 'h-[36px]',
-    1: 'h-[108px]',
-    2: 'h-[216px]',
-    3: 'h-[432px]',
-    4: 'h-[864px]',
+    0: 'h-[4px]',
+    1: 'h-[64px]',
+    2: 'h-[136px]',
+    3: 'h-[256px]',
+    4: 'h-[495px]',
     5: 'h-[0]',
     6: 'h-[0]',
   };
 
   return (
-    <li className={cn('group grid cursor-default', 'grid-cols-[135px_1fr]')}>
-      <div className={cn('flex flex-col')}>
-        {!isFirstCol && (
-          <>
-            <div
-              className={cn(
-                'border-Grey102 relative order-first w-1/2 rounded-e-md border-e border-t border-dashed',
-                isLastCol
-                  ? 'relative bottom-0 right-[90%] w-[calc(100%+40%)]'
-                  : '',
-                isLastCol
-                  ? arrowHeight[roundIndex - 1]
-                  : arrowHeight[roundIndex],
-              )}
-            >
-              <Image
-                className={cn('absolute bottom-[-1px] right-[-6px] h-3 w-3')}
-                src={'/ru/images/icons/triangle.svg'}
-                alt=""
-                width={10}
-                height={10}
-              />
-            </div>
-            <div
-              className={cn(
-                'border-Grey102 relative order-last w-1/2 rounded-e-md border-b border-e border-dashed',
-                isLastCol
-                  ? 'relative bottom-0 right-[90%] w-[calc(100%+40%)]'
-                  : '',
-                isLastCol
-                  ? arrowHeight[roundIndex - 1]
-                  : arrowHeight[roundIndex],
-              )}
-            >
-              <Image
-                className={cn(
-                  'absolute right-[-6px] top-[-1px] h-3 w-3 rotate-180',
-                )}
-                src={'/ru/images/icons/triangle.svg'}
-                alt=""
-                width={10}
-                height={10}
-              />
-            </div>
-          </>
-        )}
+    <li className={cn('group grid cursor-default',
+      isPreSemiFinalCol ? 'grid-cols-[122px_381px]':
+      (!isLastCol && !isPreLastCol) ? 'grid-cols-[122px_153px]' : '',
+    )}>
+      <div className={cn('flex flex-col', isPreLastCol ? 'mr-2' : '')}>
         <div
           className={cn(
-            'relative flex h-[72px] justify-between rounded-lg bg-card-background px-3 py-2',
-            isPreLastCol ? '-ml-2 mr-2' : '',
+            'relative flex h-[56px] justify-between rounded-lg bg-card-background px-2 py-1.5',
             className,
           )}
         >
           <div>
-            <H4>{format(time, 'HH:mm')}</H4>
+            <H4low className="text-white text-nowrap">{format(time, 'HH:mm')}</H4low>
             <p className="text-ColorsGrey26 text-nowrap text-sm">
               Мат № {mat_number}
             </p>
@@ -259,40 +273,117 @@ export function GridCard({
             <GridCardPlayerId
               player={player_1}
               isWinner={isPlayerFirstWinner && !isDraw}
+              isPreLastCol={isPreLastCol}
+              isLastCol={isLastCol}
             />
             <GridCardPlayerId
               player={player_2}
               isWinner={!isPlayerFirstWinner && !isDraw}
+              isPreLastCol={isPreLastCol}
+              isLastCol={isLastCol}
             />
           </div>
+          {(isLastCol || isPreLastCol) && (
+            <div className='text-[11px] font-black text-Grey90 flex flex-col justify-between items-center py-0.5'>
+              <p>{player_1.points}</p>  
+              <p>{player_2.points}</p>  
+            </div>
+          )}
         </div>
       </div>
       {!(isLastCol || isPreLastCol) && (
-        <div className="relative flex h-1/2 w-full items-end">
-          <div className="border-Grey102 w-full border-b border-dashed"></div>
-          <p className="relative bottom-[-8px] mt-[10px] text-nowrap px-1 text-xs font-semibold text-text-muted">
-            <span
-              className={cn(isPlayerFirstWinner && !isDraw ? 'text-white' : '')}
-            >
-              {player_1.points}
-            </span>{' '}
-            :{' '}
-            <span
+        <>
+          <div className={cn('relative flex w-full items-end',
+            fight_id % 2 === 0 ? arrowBottomShift[roundIndex] : arrowTopShift[roundIndex])}>
+            <div className="border-Grey102 w-[53px] border-b border-dashed h-full"></div>
+            <p className={cn('relative text-nowrap px-[5.5px] text-xs font-semibold text-text-muted',
+              fight_id % 2 === 0 ? 'bottom-[-8px]' : 'top-[7px]'
+            )}>
+              <span
+                className={cn(isPlayerFirstWinner && !isDraw ? 'text-white' : '')}
+              >
+                {player_1.points}
+              </span>{' '}
+              :{' '}
+              <span
+                className={cn(
+                  !isPlayerFirstWinner && !isDraw ? 'text-white' : ''
+                )}
+              >
+                {player_2.points}
+              </span>
+            </p>
+            <div
               className={cn(
-                !isPlayerFirstWinner && !isDraw ? 'text-white' : '',
+                'border-Grey102 relative w-1/2 border-e border-dashed',
+                fight_id % 2 === 0 ? 'rounded-br-md border-b' : `rounded-tr-md border-t ${arrowTop[roundIndex]}`,
+                isLastCol
+                  ? 'relative bottom-0 right-[90%] w-[calc(100%+40%)]'
+                  : '',
+                isLastCol
+                  ? arrowHeight[roundIndex - 1]
+                  : arrowHeight[roundIndex]
               )}
             >
-              {player_2.points}
-            </span>
-          </p>
-        </div>
+              <Image
+                className={cn(
+                  'absolute right-[-4px]  h-[7px] w-[7px]',
+                  fight_id % 2 === 0 ? 'rotate-180 top-[-1px]' : 'bottom-[-1px]'
+                )}
+                src={'/ru/images/icons/triangle.svg'}
+                alt=""
+                width={10}
+                height={10} />
+            </div>
+          </div>
+          <div
+            className={cn(
+              'group-last:h-0',
+              isLastCol ? spaceHeight[roundIndex - 1] : spaceHeight[roundIndex]
+            )}
+          ></div>
+          {isPreSemiFinalCol && (
+            <div className={cn('relative flex w-[151px] items-end',
+              fight_id % 2 === 0 ? `${bronzeArrowBottomShift[roundsNumber]} ${arrowBottomShift[roundIndex]}` : `${bronzeArrowTopShift[roundsNumber]} ${arrowTopShift[roundIndex]}`)}>
+              <div className="border-Grey102 w-[53px] border-b border-dashed h-full"></div>
+              <p className={cn('relative text-nowrap px-[5.5px] text-xs font-semibold text-text-muted',
+                fight_id % 2 === 0 ? 'bottom-[-8px]' : 'top-[7px]'
+              )}>
+                <span
+                  className={cn(isPlayerFirstWinner && !isDraw ? 'text-white' : '')}
+                >
+                  {player_1.points}
+                </span>{' '}
+                :{' '}
+                <span
+                  className={cn(
+                    !isPlayerFirstWinner && !isDraw ? 'text-white' : ''
+                  )}
+                >
+                  {player_2.points}
+                </span>
+              </p>
+              <div
+                className={cn(
+                  'border-Grey102 relative w-[60px] border-e border-dashed',
+                  fight_id % 2 === 0 ? 'rounded-br-md border-b' : `rounded-tr-md border-t ${bronzeArrowTop[roundsNumber]}`,
+                  bronzeArrowHeight[roundsNumber]
+                )}
+              >
+                <Image
+                  className={cn(
+                    'absolute right-[-4px]  h-[7px] w-[7px]',
+                    fight_id % 2 === 0 ? 'rotate-180 top-[-1px]' : 'bottom-[-1px]'
+                  )}
+                  src={'/ru/images/icons/triangle.svg'}
+                  alt=""
+                  width={10}
+                  height={10} />
+              </div>
+            </div>
+          )}
+        </>
       )}
-      <div
-        className={cn(
-          'group-last:h-0',
-          isLastCol ? spaceHeight[roundIndex - 1] : spaceHeight[roundIndex],
-        )}
-      ></div>
     </li>
   );
 }
@@ -300,23 +391,30 @@ export function GridCard({
 function GridCardPlayerId({
   player,
   isWinner,
+  isLastCol,
+  isPreLastCol,
 }: {
   player: GridPlayer;
   isWinner: boolean;
+  isLastCol: boolean | undefined;
+  isPreLastCol: boolean | undefined;
 }) {
   return (
     <HoverCard>
       <HoverCardTrigger>
         <div
           className={cn(
-            'bg-Grey100 relative flex justify-center rounded-md p-[6px]',
+            'bg-Grey100 relative flex justify-center items-center rounded-md p-[6px] w-[29px] h-5',
             player.first_name ? 'cursor-pointer' : '',
+            isWinner && (isPreLastCol ? 'bg-bronze': 
+            isLastCol ? 'bg-gold' : '' ),
+            !isWinner && isLastCol ? 'bg-silver' : ''
           )}
         >
-          {isWinner && (
+          {isWinner && !isLastCol && !isPreLastCol && (
             <Image
               className="absolute left-0 top-[calc(50%-5px)]"
-              src={'/images/icons/winner-marker.svg'}
+              src={'/ru/images/icons/winner-marker.svg'}
               alt=""
               width={2}
               height={10}
