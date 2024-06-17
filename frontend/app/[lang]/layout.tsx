9e62dfe5049a176427c27b5a90c94a4b79421d76
Julia Zhidkova <zhidkova.julia@gmail.com>
2024-06-17 23:50:31 +0200

    fix: change favicon

--- frontend/app/[lang]/layout.tsx
+++ frontend/app/[lang]/layout.tsx
@@ -10,11 +10,29 @@ export const metadata: Metadata = {
   title: 'Спортплатформа',
   description:
     'Онлайн-платформа для организации, регистрации и участия в соревнованиях и мероприятиях в сфере боевых искусств. Платформа обеспечивает удобное взаимодействие организаторов мероприятий, соревнующихся и зрителей, предоставляя всестороннюю функциональность для планирования, управления и участия в событиях. ',
-  icons: {
-    icon: '/ru/images/logo.svg',
-  },
-};
-
+    icons: {
+      icon: '/favicon.ico',
+      apple: '/apple-touch-icon.png',
+      other: [
+        {
+          rel: 'icon',
+          url: '/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          rel: 'icon',
+          url: '/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          rel: 'manifest',
+          url: '/site.webmanifest',
+        },
+      ],
+    },
+  };
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
 }
@@ -30,6 +48,12 @@ export default async function RootLayout({
 
   return (
     <html lang={params.lang}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${inter.className} relative mx-auto w-full bg-primary-background antialiased`}
         // suppressHydrationWarning={false} // To switch off warnings
