29b076645bfa871faa275ca88ea3ad3733748759
Aleksei Smirnov <sibusky@yandex.ru>
2024-06-19 15:50:19 +0400

    feat: add feedback functional

--- frontend/app/[lang]/(unprotected)/feedback/feedback-form.tsx
+++ frontend/app/[lang]/(unprotected)/feedback/feedback-form.tsx
@@ -5,27 +5,29 @@ import {
   TypeFieldsetData,
 } from '@/components/forms/custom-fieldset';
 import { CustomForm } from '@/components/forms/custom-form';
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { Locale } from '@/i18n.config';
-import { Button } from '@/components/ui/button';
-import { Spinner } from '@/components/spinner';
 import { Form } from '@/components/ui/form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { ButtonsBlock } from '@/components/auth/buttons-block';
 import { CustomLink } from '@/components/custom-link';
+import { useFormState } from 'react-dom';
+import { sendFeedback } from '@/lib/actions/feedback';
+import { SubmitActionButton } from '@/components/forms/submit-action-button';
+import { toast } from 'sonner';
+import { BackButton } from '@/components/back-button';
 
-const formSchema = z
-  .object({
-    name: z.string(),
-    email: z
-      .string()
-      .regex(
-        /^[a-zA-Zа-яА-Я0-9_.+-]+@[a-zA-Zа-яА-Я0-9-]+\.[a-zA-Zа-яА-Я0-9-.]+$/,
-      ),
-      message: z.string(),
-    });
+const formSchema = z.object({
+  name: z.string(),
+  email: z
+    .string()
+    .regex(
+      /^[a-zA-Zа-яА-Я0-9_.+-]+@[a-zA-Zа-яА-Я0-9-]+\.[a-zA-Zа-яА-Я0-9-.]+$/,
+    ),
+  message: z.string().min(10, 'Минимальная длина 10 символов'),
+});
 
 const feedbackFieldset: TypeFieldsetData<z.infer<typeof formSchema>> = {
   fields: [
@@ -34,28 +36,24 @@ const feedbackFieldset: TypeFieldsetData<z.infer<typeof formSchema>> = {
       name: 'name',
       placeholder: 'Введите имя',
       label: 'Имя',
-      defaultValue: '',
     },
     {
       type: 'email',
       name: 'email',
       placeholder: 'Ваша почта',
       label: 'Электронная почта',
-      defaultValue: '',
     },
     {
       type: 'textarea',
       name: 'message',
-      placeholder: 'Введите сообщение',
+      placeholder: 'Введите сообщение (минимум 10 символов)',
       label: 'Сообщение',
-      defaultValue: '',
     },
   ],
 };
 
 export function FeedbackForm({ lang }: { lang: Locale }) {
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, dispatch] = useFormState(sendFeedback, undefined);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -66,18 +64,25 @@ export function FeedbackForm({ lang }: { lang: Locale }) {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>): void {
-    setIsLoading(true);
-    setErrorMessage('');
-    console.log('feedback logic', values);
-  }
+  useEffect(() => {
+    if (errorMessage === '200') {
+      toast.success('Ваше сообщение отправлено');
+    }
+    if (errorMessage === '400') {
+      toast.success(
+        'Произошла ошибка, ваше сообщение не отправлено. Попробуйте повторить позже.',
+      );
+    }
+  }, [errorMessage]);
+
   return (
     <Form {...form}>
-      <CustomForm onSubmit={form.handleSubmit(onSubmit)}>
+      <CustomForm action={dispatch}>
         <CustomFieldset
           form={form}
           lang={lang}
           fieldsetData={feedbackFieldset}
+          isFieldsetDisabled={errorMessage === '200'}
         />
         <ButtonsBlock>
           <CustomLink
@@ -87,16 +92,14 @@ export function FeedbackForm({ lang }: { lang: Locale }) {
           >
             <p className="text-xs">На главную</p>
           </CustomLink>
-          <Button
-            className="flex w-full gap-3 px-9 sm:w-auto"
-            variant="ruchampDefault"
-            type="submit"
-            size="lg"
-            disabled={false}
-          >
-            {false && <Spinner className="h-6 w-6" />}
-            Отправить
-          </Button>
+          {errorMessage === '200' ? (
+            <BackButton variant={'ruchampDefault'} />
+          ) : (
+            <SubmitActionButton
+              text={'Отправить'}
+              isValid={form.formState.isValid}
+            />
+          )}
         </ButtonsBlock>
       </CustomForm>
     </Form>
