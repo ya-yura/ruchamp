cb0be289f0844c5acbe1aff89ae131237f047c28
Alena Lyalyushkina <pluto_16@mail.ru>
2024-07-05 19:06:08 +1000

    fix: fix profile bug

--- frontend/app/[lang]/(protected)/profile/page.tsx
+++ frontend/app/[lang]/(protected)/profile/page.tsx
@@ -34,6 +34,11 @@ export default async function AthleteProfile({
     fetchAthleteMatches(token),
     fetchAthleteTeams(token),
   ]);
+
+  // console.log('teams ===>', teams);
+
+
+
   const user: UserInfo | null = session
     ? {
         basicInfo: session.user[1],
