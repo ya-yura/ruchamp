99b7afad26073c9bd51e8391a50552180b384148
olga_k_qschool <ms.olgasmith.jj@mail.ru>
2024-07-08 20:26:56 +0300

    feat-add-join-team

--- frontend/app/[lang]/(unprotected)/team/[id]/team-action-buttons.tsx
+++ frontend/app/[lang]/(unprotected)/team/[id]/team-action-buttons.tsx
@@ -1,10 +1,80 @@
-import { Button } from '@/components/ui/button';
-import React from 'react';
+'use client';
 
-export function TeamActionButtons() {
+import { Spinner } from '@/components/spinner';
+import { Button } from '@/components/ui/button';
+import { getSession } from '@/lib/actions/auth';
+import { fetchAthleteTeams, joinTeam } from '@/lib/data';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+
+interface TeamActionButtonsProps {
+  id: string;
+  teamName: string;
+}
+
+export function TeamActionButtons({ id, teamName }: TeamActionButtonsProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isUserInTeam, setIsUserInTeam] = useState<boolean>(false);
+
+  const submitJoinTeam = async () => {
+    setIsLoading(true);
+    const session = await getSession();
+    const token = session?.token;
+    if (token) {
+      joinTeam(token, id)
+        .then(() => {
+          toast.success(`Поздравляем! Вы вступили в команду - ${teamName}`);
+          return getUsersTeamData();
+        })
+        .catch((err) => {
+          console.log('Ошибка при добавлении в команду: ', err);
+          toast.error('Что-то пошло не так');
+        });
+    }
+  };
+
+  const getUsersTeamData = async () => {
+    setIsLoading(true);
+    const session = await getSession();
+    const token = session?.token;
+    if (token) {
+      fetchAthleteTeams(token)
+        .then((athleteTeams) => {
+          if (athleteTeams) {
+            const isInTeam = athleteTeams.find((team) => team.team_id === +id);
+            setIsUserInTeam(!!isInTeam);
+          }
+        })
+        .catch((err) => {
+          console.log('Ошибка при получении данных: ', err);
+          toast.error('Не удалось загрузить данные');
+        })
+        .finally(() => setIsLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    getUsersTeamData();
+  }, []);
+
   return (
     <div className="mb-[87px] flex gap-6">
-      <Button variant="ruchampDefault">Вступить</Button>
+      {isUserInTeam ? (
+        <Button variant="transparentGreen">
+          {isLoading && <Spinner className="h-6 w-6" />}
+          &#10003; Вы в команде
+        </Button>
+      ) : (
+        <Button
+          variant="ruchampDefault"
+          onClick={submitJoinTeam}
+          disabled={isLoading ? true : false}
+        >
+          {isLoading && <Spinner className="h-6 w-6" />}
+          Вступить
+        </Button>
+      )}
+
       <Button variant="ruchampTransparent">Переназначить капитана</Button>
     </div>
   );
